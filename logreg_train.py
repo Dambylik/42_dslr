aae06154ca35e207022f667cc924b30be7a1daac
Olga <dambylik@gmail.com>
2025-12-14 15:57:06 +0100

    init

---
+++ logreg_train.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+    """Main programm"""
+    
+    theta0, theta1 = load_model()
+    
+    print("Logistic Regression project")
+    print("---------------------------")
+
+    
+
+if __name__ == "__main__":
+    main()
