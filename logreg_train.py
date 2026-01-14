6b230797ed55d62eb66b1f39cb27032097aab11e
OK <dambylik@gmail.com>
2026-01-14 12:34:27 +0100

    implement logistic regression sigmoid function and main structure

--- logreg_train.py
+++ logreg_train.py
@@ -1,12 +1,22 @@
 import sys
+import math
+
+def sigmoid(z):
+    """sigmoid function : σ(z) = 1 / (1 + e⁻ᶻ)"""
+    if z >= 0:
+        return 1 / (1 + math.exp(-z))
+    else:
+        exp_z = math.exp(z)
+        return exp_z / (1 + exp_z)
+
+
 
 def main():
     """Main programm"""
-    
-    theta0, theta1 = load_model()
-    
+        
     print("Logistic Regression project")
     print("---------------------------")
+    print(sigmoid(-5))
 
     
 
--- pair_plot.py
+++ pair_plot.py
@@ -1,1 +1,35 @@
-#to do
+import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def load_data(path):
+    df = pd.read_csv(path)
+
+    drop_cols = ["Index", "First Name", "Last Name", "Birthday"]
+    df = df.drop(columns=drop_cols)
+    return df
+
+
+def plot_pair(df):
+    sns.pairplot(
+        df,
+        hue="Hogwarts House",
+        diag_kind="hist",
+        plot_kws={"alpha": 0.6, "s": 15}
+    )
+    plt.show()
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python pair_plot.py dataset.csv")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    df = load_data(path)
+    plot_pair(df)
+
+if __name__ == "__main__":
+    main()
